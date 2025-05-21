6f42a7f65356df1b331bf72862788147079ba204
Look to la Luna <114422891+pekpuch@users.noreply.github.com>
2025-05-21 22:03:16 +0700

    Update test_contract_parser.py

--- test/test_doc_parser/test_contract_parser.py
+++ test/test_doc_parser/test_contract_parser.py
@@ -1,8 +1,13 @@
 from legal_doc_inspector.doc_parser.contract_parser import contract_parser
 
-def test_contract_parser():
+def test_contract_parser(path, question):
     test_contract_parser = contract_parser()
-    question = "До какого числа пользователь должен внести оплату"
-    chunks = test_contract_parser.pdf_to_text("path")
+    chunks = test_contract_parser.pdf_to_text(path)
     response = test_contract_parser.parse(chunks, question)
     return response
+
+path = ""
+question = "До какого числа пользователь должен внести оплату"
+
+result = test_contract_parser(path, question)
+print(result)
