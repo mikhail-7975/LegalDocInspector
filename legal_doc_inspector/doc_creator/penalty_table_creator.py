5ca90fd8b42bba218ea6bb42c6b5ff45d86f8805
Kirill <@salainennn>
2025-05-29 22:39:41 +0700

    add styles for some elements in table

--- legal_doc_inspector/doc_creator/penalty_table_creator.py
+++ legal_doc_inspector/doc_creator/penalty_table_creator.py
@@ -1,20 +1,24 @@
 from collections import defaultdict
 from pathlib import Path
 from datetime import date
-
-from docx.table import Table
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.table import Table, _Cell, _Row, _Column
 from docx import Document
-from docx.shared import Pt
+from docx.shared import Pt, Cm
 from docx.enum.section import WD_ORIENT
+from docx.oxml import OxmlElement
+from docx.oxml.ns import qn
 
 class PenaltyTableCreator:
     
     def __init__(self):
         self.doc = Document()
     
+
     def save_doc(self, path_to_save:Path):
         self.doc.save(str(path_to_save))
     
+
     def _create_table_title(self, contract_number:str, start_date, end_date ):
         doc = self.doc
         
@@ -83,6 +87,7 @@ class PenaltyTableCreator:
         
         return table
     
+
     def group_by_month(self, data):
         """
         принимает результат калькулятора пени, 
@@ -115,7 +120,7 @@ class PenaltyTableCreator:
                 
                 for key, value in payment.items():
                     if key not in payments_seen:
-                        print(key)
+                        # print(key)
                         payments_seen.add(key)
                         combined.append({'type': 'payment', 'data': {key:value}})
                 
@@ -168,17 +173,32 @@ class PenaltyTableCreator:
                     new_row_cells[0].merge(new_row_cells[1])
                     new_row_cells[2].merge(new_row_cells[3])
 
-                    new_row_cells[2].text = f'{debt}'
+                    
+                    self._put_text_into_table_cell(text=f'{debt}',
+                                                   cell=new_row_cells[2],)
 
-                    new_row_cells[4].text = f'{start_date}'
-                    new_row_cells[5].text = f'{end_date}'
-                    new_row_cells[6].text = f'{days_count}'
+                    self._put_text_into_table_cell(text=f'{start_date}',
+                                                   cell=new_row_cells[4] )
 
-                    new_row_cells[7].text = '9,50%'
-                    new_row_cells[8].text = rate
+                    self._put_text_into_table_cell(text=f'{end_date}',
+                                                   cell=new_row_cells[5])
+                    
+                    self._put_text_into_table_cell(text=f'{days_count}',
+                                                   cell=new_row_cells[6])
+                    
+                    self._put_text_into_table_cell(text='9,50%',
+                                                   cell=new_row_cells[7])
 
-                    new_row_cells[9].text = f'{debt} × {days_count} × {rate} × 9,5%'
-                    new_row_cells[10].text = f'{penalty} р.'
+                    self._put_text_into_table_cell(text=rate,
+                                                   cell=new_row_cells[8])
+                    
+                    self._put_text_into_table_cell(text=f'{debt} × {days_count} × {rate} × 9,5%',
+                                                   cell=new_row_cells[9])
+
+                    self._put_text_into_table_cell(text=f'{penalty} р.',
+                                                   cell=new_row_cells[10],
+                                                   orient='right')
+
                     new_rows_for_month.append(new_row)
 
                 if period_or_payment['type'] == 'payment':
@@ -190,26 +210,43 @@ class PenaltyTableCreator:
 
                     new_row_cells[0].merge(new_row_cells[1])
                     new_row_cells[2].merge(new_row_cells[3])
+                    self._put_text_into_table_cell(text=f'-{amount}',
+                                                   cell=new_row_cells[2])
 
-                    new_row_cells[2].text = f'-{amount}'
-
-                    new_row_cells[4].text = f'{date_of_payment}'
+                    self._put_text_into_table_cell(text=f'{date_of_payment}',
+                                                   cell=new_row_cells[4])
+                    
+                    self._put_text_into_table_cell(text="погашение части долга",
+                                                   cell=new_row_cells[5],
+                                                   orient='left')
                     new_row_cells[5].merge(new_row_cells[10])
-                    new_row_cells[5].text = "погашение части долга"
                     new_rows_for_month.append(new_row)
             
             itog_row = table.add_row()
             itog_row_cells = itog_row.cells
             itog_row_cells[0].merge(itog_row_cells[1])
             itog_row_cells[2].merge(itog_row_cells[3])
-            itog_row_cells[9].text = 'Итого'
-            itog_row_cells[10].text = f'{itogo} р.'
+            self._put_text_into_table_cell(text="Итого",
+                                           cell=itog_row_cells[9],
+                                           need_bold=True,
+                                           orient='right')
+            
+            self._put_text_into_table_cell(text=f'{itogo} р.',
+                                           cell=itog_row_cells[10],
+                                           need_bold=True,
+                                           orient='right')
+            
             new_rows_for_month.append(itog_row)
             new_rows_for_month[0].cells[0].merge(new_rows_for_month[-1].cells[0])
-            new_rows_for_month[0].cells[0].text = f'{month_key}'
-        
+            
+            self._put_text_into_table_cell(text=f'{month_key}',
+                                           cell=new_rows_for_month[0].cells[0],
+                                           need_bold=True,
+                                           need_vertical_orient=True)
+            self._set_row_bottom_border(new_rows_for_month[-1])
         return table
     
+
     def _rate_float_to_decimal(self,rate):
         """
         переводит число с плавающей точкой в обыкновенную дробь
@@ -228,6 +265,7 @@ class PenaltyTableCreator:
         if rate==fourth:
             return "0"
         
+
     def create_penalty_table_from_json(self, name, data, contract_number, start_date, end_date):
         table = self._create_table_title(contract_number, start_date, end_date)
         list_of_periods = self.group_by_month(data)
@@ -236,7 +274,8 @@ class PenaltyTableCreator:
 
         return self.convert_datetime_keys(list_of_periods)
     
-    def convert_datetime_keys(self,obj):
+
+    def convert_datetime_keys(self, obj):
         if isinstance(obj, dict):
             new_dict = {}
             for k, v in obj.items():
@@ -254,3 +293,87 @@ class PenaltyTableCreator:
             return obj.strftime("%d.%m.%Y")
         else:
             return obj
+        
+
+    def _put_text_into_table_cell(self, text:str, cell:_Cell, need_bold=False, need_italic=False, orient="center", need_vertical_orient=False, need_gray_bgc=False):
+        paragraph = cell.paragraphs[0]
+        run = paragraph.add_run(text)
+        run.bold = need_bold
+        run.italic = need_italic
+        run.font.name = 'Times New Roman'
+
+        element = run._element
+        rPr = element.get_or_add_rPr()
+        rFonts = rPr.get_or_add_rFonts()
+        rFonts.set(qn('w:ascii'), 'Times New Roman')
+        rFonts.set(qn('w:hAnsi'), 'Times New Roman')
+        rFonts.set(qn('w:eastAsia'), 'Times New Roman')  
+        rFonts.set(qn('w:cs'), 'Times New Roman')
+        # run._element.rPr.rFonts.set('w:eastAsia', 'Times New Roman')  # для корректного отображения в Word
+        run.font.size = Pt(9)
+
+        match orient:
+            case "center":
+                paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+            case "left":
+                paragraph.alignment = WD_ALIGN_PARAGRAPH.LEFT
+            case "right":
+                paragraph.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+        
+        if need_vertical_orient:
+            self._set_cell_vertical_alignment(cell=cell)
+
+        if need_gray_bgc:
+            self._set_cell_background(cell=cell)
+
+
+    def _set_cell_vertical_alignment(self, cell:_Cell, align="center"):
+        tc = cell._tc
+        tc_pr = tc.get_or_add_tcPr()
+        tag = tc_pr.xpath('w:vAlign')
+        if tag:
+            el = tag[0]
+            el.set(qn('w:val'), align)
+        else:
+            valign = OxmlElement('w:vAlign')
+            valign.set(qn('w:val'), align)
+            tc_pr.append(valign)
+
+
+    def _set_cell_background(self, cell:_Cell, color_hex = "#cccccc"):
+        tc_pr = cell._tc.get_or_add_tcPr()
+        shading_elm = OxmlElement('w:shd')
+        shading_elm.set(qn('w:fill'), color_hex)
+        tc_pr.append(shading_elm)
+
+
+    def _set_row_bottom_border(self, row:_Row, color="000000", size=4):
+        """ 
+        :param row: объект строки (Row)
+        :param color: цвет в HEX без решётки (#), например "000000" — чёрный
+        :param size: толщина линии (в 1/8 pt, например 4 = 0.5pt, 6 = 0.75pt)
+        """
+        tr = row._tr
+        tc_borders = OxmlElement('w:tblBottomBorders')
+
+        bottom_border = OxmlElement('w:bottom')
+        bottom_border.set(qn('w:val'), 'single')
+        bottom_border.set(qn('w:sz'), str(size))
+        bottom_border.set(qn('w:color'), color)  
+
+        tc_borders.append(bottom_border)
+
+        for el in tr.xpath('w:tblBottomBorders'):
+            tr.remove(el)
+
+        tr.append(tc_borders)
+    
+
+    def _apply_height_for_table(self, table:Table, height:float):
+        for row in table.rows:
+            row.height = Cm(height)
+        
+        return table
+    
+    def _apply_width_for_column(self, column:_Column, width:float):
+        column.width = Cm(width)
