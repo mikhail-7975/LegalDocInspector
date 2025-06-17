599a30c6e75aed5e1f2d54523b9f449ba05a0a7c
Kirill <@salainennn>
2025-06-17 12:06:01 +0700

    parser parse original names

--- legal_doc_inspector/app/utils/parse_info_by_inn.py
+++ legal_doc_inspector/app/utils/parse_info_by_inn.py
@@ -9,5 +9,5 @@ def parse_html(inn):
     kpp = soup.find('div', {'id': 'kpp'}).text.strip()
     ogrn = soup.find('span', {'id': 'ogrn'}).text.strip()
     short_name = soup.find('h1', {'id': 'short_name'}).text.strip()
-    full_name = soup.find('h2', {'id': 'full_name'}).text.strip().title()
+    full_name = soup.find('h2', {'id': 'full_name'}).text.strip()
     return full_name, short_name, address, kpp, ogrn
