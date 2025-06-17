25471b1c8e3cf6ece3bf980950e39e5163351c65
Kirill <@salainennn>
2025-06-17 14:46:51 +0700

    fix addres formatting

--- legal_doc_inspector/app/utils/parse_info_by_inn.py
+++ legal_doc_inspector/app/utils/parse_info_by_inn.py
@@ -10,8 +10,16 @@ def parse_html(inn):
     ogrn = soup.find('span', {'id': 'ogrn'}).text.strip()
     short_name = soup.find('h1', {'id': 'short_name'}).text.strip()
     full_name = soup.find('h2', {'id': 'full_name'}).text.strip()
-    return replace_quotes(full_name), replace_quotes(short_name), address, kpp, ogrn
+    return replace_quotes(full_name), replace_quotes(short_name), clean_address(address), kpp, ogrn
 
 def replace_quotes(text:str):
     text = re.sub(r'"([^"]*)"', r'«\1»', text)
+    return text
+
+def clean_address(text: str) -> str:
+    
+    text = re.sub(r',\s*,', ',', text)        
+    text = re.sub(r'\s+,', ',', text)          
+    text = re.sub(r',(?=[^ \w])', '', text)    
+    text = re.sub(r'\s+', ' ', text).strip() 
     return text
